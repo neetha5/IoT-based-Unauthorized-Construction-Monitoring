e6b6830a064cf9eed54670d46c2ebd29e20c156c
neetha5 <neethacn2004@gmail.com>
2026-03-15 23:06:43 +0530

    Create app3.py

---
+++ app3.py
@@ -0,0 +1,40 @@
+pipeline {
+  agent any
+
+  stages {
+
+    stage('Clone') {
+      steps {
+        git url: 'https://github.com/neetha5/jenkins-simple-demo.git',
+          branch: 'main'
+      }
+    }
+    stage('Run Script'){
+      steps {
+        sh 'chmod +x script.sh'
+        sh './script.sh'
+      }
+    }
+  }
+}
+
+
+
+#!/bin/bash
+echo "Hello from GitHub Project!"
+echo "CI/CD Pipeline Executed Successfully"
+
+sudo apt install
+sudo apt install openjdk-21-jdk -y
+wget https://get.jenkins.io/war-stable/latest/jenkins.war
+java -jar jenkins.war --httpPort=8090
+
+to stop
+rm jenkins.war
+sudo rm-rf ~/.jenkins
+
+ngork --version
+sudo apt install snapd -y
+sudo snap install ngrok
+
+paste url /github-webhook/
